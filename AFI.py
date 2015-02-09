ced708acc9732253ad34edcbed7110c536f62588
Adam Crick <adam.crick@hotmail.com>
2015-02-08 23:39:25 -0500

    Fixed Tab Errors
    Tabbing error suck

--- AFI.py
+++ AFI.py
@@ -12,26 +12,25 @@ import mathAFI
 
 #Takes input string, splits the spaces, and figures out what to do with it
 def execCmd(inputStr):
-
     return mathAFI.calculate(inputStr)
-
 
 #Main instructional function.
 def main():
     version = "0.0.2"
     if len(sys.argv) == 1:
-    	print("Welcome to AFI V",version,"! Enter an expression below:", sep='')
-    	while 1:
-        	inputStr = input("|")
-        	print("|" + str(execCmd(inputStr)), sep='')
+        print("Welcome to AFI V",version,"! Enter an expression below:", sep='')
+        while 1:
+            inputStr = input("|")
+            print("|" + str(execCmd(inputStr)), sep='')
     elif len(sys.argv) < 3:
-    	filename = sys.argv[1]
-        if ".afi" in filename:
+        filename = sys.argv[1]
+        fileext = ".afi"
+        if fileext in filename:
             file = open(filename, r)
         elif "." in filename:
             print("ERROR: wrong file extension, please use .afi")
         else:
             file = open(filename + ".afi", r)
     else:
-    	print("Usage: python AFI.py <filename>")
+        print("Usage: python AFI.py <filename>")
 main()
