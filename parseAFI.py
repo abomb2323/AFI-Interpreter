2fdc4a4811edc249f0d28e0d0c7f68d6903f08bb
Adam Crick <adam.crick@hotmail.com>
2015-02-09 15:39:38 -0500

    Added Parser Stuff
    Parser still not workable, should be done by tonight

--- parseAFI.py
+++ parseAFI.py
@@ -6,3 +6,17 @@ AFI Parser
 """
 
 from rply import ParserGenerator
+
+import astAFI
+
+def generateParser():
+
+	afiParser = ParserGenerator(
+	#A list of token names accepted by the Parser
+		["LBRACKET", "RBRACKET", "LBRACE", "RBRACE", "LPARENS", "RPARENS",
+	 	 "QUOTE", "IF", "THEN", "ELSE", "ELIF", "WHILE", "DO", "NUMBER",
+	 	 "WORD", "EQUAL", "ADD", "SUB", "MULT", "DIV", "POW"
+		]
+	)
+
+	
